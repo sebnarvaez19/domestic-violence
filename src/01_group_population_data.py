3f9cf71233fb2061dafb2a582a9eb389cc6286ce
srnarvaez <sebnarvaez19@gmail.com>
2023-01-09 18:10:22 -0500

    comment lines

--- src/01_group_population_data.py
+++ src/01_group_population_data.py
@@ -2,32 +2,49 @@ import os
 import pandas as pd
 from tabula import read_pdf
 
+# Path to load and save data
 codes_path = "data/external/Codificación de Municipios por Departamento.pdf"
 data_path = "data/external/national_census_data/"
 save_path = "data/processed/population.csv"
 
+# Columns for codes dataset
 codes_cols = ["U_DPTO", "U_MPIO", "Departamento", "Municipio"]
-pop_data = []
 
-files = os.listdir(data_path)
+def main():
+    # List to store all population datasets
+    pop_data = []
 
-for f in files:
-    df = pd.read_csv(data_path+f)
-    df = df.groupby(by=["U_DPTO", "U_MPIO"])[["HA_TOT_PER"]].sum()
-    df = df.reset_index()
+    # Get the names of all population datasets
+    files = os.listdir(data_path)
 
-    pop_data.append(df)
+    # For each files, read it, group data by municipio and departamento and sum it
+    for f in files:
+        df = pd.read_csv(data_path+f)
+        df = df.groupby(by=["U_DPTO", "U_MPIO"])[["HA_TOT_PER"]].sum()
+        df = df.reset_index()
 
-pop_data = pd.concat(pop_data, ignore_index=True)
+        # Add data to population list
+        pop_data.append(df)
 
-codes = read_pdf(codes_path, pages="all", pandas_options={"columns": codes_cols})
+    # Convert list in a dataframe
+    pop_data = pd.concat(pop_data, ignore_index=True)
 
-codes[0] = codes[0][1:]
-codes[0].U_DPTO = codes[0].U_DPTO.astype("int64")
-codes[0].U_MPIO = codes[0].U_MPIO.astype("int64")
+    # Load all codes and define the column names
+    codes = read_pdf(codes_path, pages="all", pandas_options={"columns": codes_cols})
 
-codes = pd.concat(codes, ignore_index=True)
+    # Delete first row and change the dtype of the code columns of first dataframe
+    codes[0] = codes[0][1:]
+    codes[0].U_DPTO = codes[0].U_DPTO.astype("int64")
+    codes[0].U_MPIO = codes[0].U_MPIO.astype("int64")
 
-pop_data = pop_data.merge(codes, on=["U_DPTO", "U_MPIO"])
+    # Merge all codes dataframes
+    codes = pd.concat(codes, ignore_index=True)
 
-pop_data.to_csv(save_path)
+    # Add the departamentos and municipios to population data
+    pop_data = pop_data.merge(codes, on=["U_DPTO", "U_MPIO"])
+
+    # Save population data
+    pop_data.to_csv(save_path)
+
+if __name__ == "__main__":
+    main()
